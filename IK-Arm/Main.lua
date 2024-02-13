ca33c51383fd8515b21bef9fe47699d8b4daab9b
TheIncgi <theincgi@gmail.com>
2024-02-12 20:56:36 -0500

    fix import

--- IK-Arm/Main.lua
+++ IK-Arm/Main.lua
@@ -1,7 +1,7 @@
 require("TheIncgi/Plasma-Projects/IK-Arm/libs/MultiTaskBase")
 _TASKS = {} --remove example task
 
-require("TheIncgi/Plasma-Projects/IK-Arm/IK-Arm/UI.lua")
+require("TheIncgi/Plasma-Projects/IK-Arm/IK-Arm/UI")
 
 
 
