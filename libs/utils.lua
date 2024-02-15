5fbaadc32678bf7bf927a988369661e7ee050c71
TheIncgi <theincgi@gmail.com>
2024-02-15 01:31:05 -0500

    more debug info

--- libs/utils.lua
+++ libs/utils.lua
@@ -100,7 +100,7 @@ function utils.kwargs( argInfo, ... )
   print(argInfo)
   print(table.serialize(argInfo))
   for i, arg in ipairs(argInfo) do
-    print("ARG: "..i.." - "..type(arg))
+    print("ARG: "..i.." - "..type(arg).." = "..tostring(arg))
     local argNames, argTypes, default, requiredTypes = argSplit( arg )
     
     if not argNames[1] then
