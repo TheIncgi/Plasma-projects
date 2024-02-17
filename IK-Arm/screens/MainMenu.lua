acd68c049ece9e85a4e59fe0a68520fc3433d4a5
TheIncgi <theincgi@gmail.com>
2024-02-16 21:14:40 -0500

    new buttons + cleanup

--- IK-Arm/screens/MainMenu.lua
+++ IK-Arm/screens/MainMenu.lua
@@ -1,16 +1,52 @@
-print("Creating Main Menu (build 4)")
+print("Creating Main Menu (build 5)")
 
 local Screen = require"TheIncgi/Plasma-projects/IK-Arm/IK-Arm/screens/Screen"
 local Button = require"TheIncgi/Plasma-projects/IK-Arm/IK-Arm/screens/Button"
+local Text   = require"TheIncgi/Plasma-projects/IK-Arm/IK-Arm/screens/Text"
+
+local x = (Screen.WIDTH / 4) - (Screen.WIDTH / 2)
+local reducedHeight = (Screen.HEIGHT * .9)
+local paddingSize = (Screen.HEIGHT * .02)
+local nElements = 4
+local elemHeight = (reducedHeight - math.max(0,paddingSize*nElements-1) ) / nElements
+local halfWidth = Screen.WIDTH / 2
+
+local y = paddingSize
+title = Text:new{
+  x = x,
+  y = y,
+  width = halfWidth,
+  height = elemHeight,
+  text = "Main Menu"
+}
+y = y + elemHeight + paddingSize
+
+dataButton = Button:new({
+  x = x,
+  y = y,
+  width = halfWidth,
+  height = elemHeight,
+  text = "Gen Data"
+})
+y = y + elemHeight + paddingSize
+
+trainButton = Button:new({
+  x = x,
+  y = y,
+  width = halfWidth,
+  height = elemHeight,
+  text = "Train"
+})
+y = y + elemHeight + paddingSize
 
 testButton = Button:new({
-  x = 5,
-  y = 5,
-  width = 20,
-  height = 20,
-  text = "Foo"
+  x = x,
+  y = y,
+  width = halfWidth,
+  height = elemHeight,
+  text = "Test"
 })
 
-local mainMenu = Screen:new(testButton)
+local mainMenu = Screen:new(dataButton, trainButton, testButton)
 
 return mainMenu
--- java/DataCollector/src/main/java/com/theincgi/DataCollector/data/IKArmModel.java
+++ java/DataCollector/src/main/java/com/theincgi/DataCollector/data/IKArmModel.java
@@ -5,10 +5,8 @@ import static jakarta.persistence.FetchType.LAZY;
 
 import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.OneToMany;
